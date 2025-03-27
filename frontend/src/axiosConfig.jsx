33a6f6124e9b2ae1f944ba65df6cfe699470451e
Evette.Hsieh <n11736313@qut.edu.au>
2025-03-27 18:24:37 +1000

    Update axios config

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5001/api', // local
-  //baseURL: 'http://3.26.96.188:5001', // live
+  baseURL: 'http://3.26.52.110', 
+  //baseURL: 'http://3.26.52.110:5001', // live
   headers: { 'Content-Type': 'application/json' },
 });
 
